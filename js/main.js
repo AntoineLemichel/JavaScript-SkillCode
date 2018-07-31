101f1f8f5f17479f7bbd124c4b0a3193d2d8846f
Andrew <andrew@MacBook-Pro-de-Andrew.local>
2018-07-31 09:52:47 +0200

    Adding exercie n°2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 // Exercice n°1 : Hello World.
 console.log("Excercice n°1 : Hello World");
 console.log("Hello World");
+var world = "Hello world";
+console.log(world, "ceci est le message avec la variable de type :", typeof(world));
+console.log("==================");
+
+// Exercice n°2 : Calcul divers.
+console.log("Excercice n°2 : Calcul divers");
+console.log(3*3);
+console.log(12/0);
+console.log(4+9+78);
+console.log(12-7);
+console.log(5^4);
 console.log("==================");
 
 
